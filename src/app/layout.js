d2f0d540e8ab3204e4e32821420939329c5f4877
Berk Uçar <berkucar0901@gmail.com>
2025-01-30 20:22:35 +0300

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import localFont from "next/font/local";
-import "./globals.css";
 import Sidebar from "../../components/Sidebar.jsx";
 
 export default function RootLayout({ children }) {
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import Sidebar from "../../components/Sidebar";
 
 export default function Home() {
